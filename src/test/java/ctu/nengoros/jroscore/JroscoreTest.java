10b5089780a2763c015281061bc8eb91ace895aa
Jaroslav Vitku <jarda.vitku@gmail.com>
2013-10-24 16:20:21 +0200

    added better test for launching Jroscore, this approach will be used for testing other projects too

--- src/test/java/ctu/nengoros/jroscore/JroscoreTest.java
+++ src/test/java/ctu/nengoros/jroscore/JroscoreTest.java
@@ -2,21 +2,32 @@ package ctu.nengoros.jroscore;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import ctu.nengoros.Jroscore;
 
 public class JroscoreTest {
 
-	@Test
-	public void test() {
-		
-		Jroscore jr = new Jroscore();
+	Jroscore jr;
+	
+	@Before
+	public void startCore(){
+		jr = new Jroscore();
 		jr.parseURI(new String[]{});
 		
 		jr.start();
 		assertTrue(jr.isRunning());
-		
+	}
+	
+	@Test
+	public void test() {
+		assertTrue(jr.isRunning());
+	}
+	
+	@After
+	public void stopCore(){
 		jr.shutDown();
 		assertFalse(jr.isRunning());
 	}
