66d95be557836a9dcc31a7f42cd37ca7136b7199
Jaroslav Vitku <vitkujar@fel.cvut.cz>
2013-11-02 21:19:27 +0100

    modified checking if a node is ready

--- src/main/java/ctu/nengoros/nodes/topicParticipant/TopicParticipants.java
+++ src/main/java/ctu/nengoros/nodes/topicParticipant/TopicParticipants.java
@@ -15,32 +15,37 @@ import static org.junit.Assert.*;
 public class TopicParticipants {
 
 	private ArrayList<TopicParticipantInterface> participants;
-	
+
 	private volatile boolean allReady = false;
 	private volatile boolean allAdded = false;	
-	
+
 	private int maxwait = 4000, wait=155;	//ms
-	
+
 	public TopicParticipants(){
 		participants = new ArrayList<TopicParticipantInterface>();
 	}
-	
+
 	/**
 	 * This has to be called after adding the last participant indicating
 	 * that we can wait for all to be ready (asynchronous access).
 	 */
 	public void allParticipantsAdded(){ allAdded = true; }	
-	
+
 	/**
 	 * Wait for all participants to be ready (just registered in the network, or connected somewhere too)
 	 */
 	public void awaitAllready(){
+
+		if(allAdded && participants.size() == 0){
+			allReady = true;
+			return;
+		}
 		
 		if(allReady)
 			return;
-		
+
 		this.awaitAllAdded();
-		
+
 		for(int i=0; i<participants.size(); i++){
 			if(!participants.get(i).isReady()){
 				participants.get(i).awaitReady();
@@ -48,7 +53,7 @@ public class TopicParticipants {
 		}
 		allReady = true;
 	}
-	
+
 	private void awaitAllAdded(){
 		int poc = 0;
 		while(!allAdded){
@@ -66,8 +71,8 @@ public class TopicParticipants {
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * Add new participant to the list. 
 	 * 
@@ -77,6 +82,6 @@ public class TopicParticipants {
 		this.allReady = false;
 		participants.add(participant);
 	}
-	
+
 	public boolean allReady(){ return this.allReady; }
 }
